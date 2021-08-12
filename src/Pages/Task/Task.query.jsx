fce806409c0d91b3eeba74d043461c3f7889ab0a
Joe Mullally <joe.mullally@sonocent.com>
2021-08-12 08:27:39 +0100

    Changed the graphql query

--- src/Pages/Task/Task.query.jsx
+++ src/Pages/Task/Task.query.jsx
@@ -3,14 +3,19 @@ import gql from 'graphql-tag';
 
 export default graphql(
     gql`
-        query allRewards($page: Int!) {
-            allRewards(page: $page) {
+        query allRewards($page: Int!, $campaign_id: ID!) {
+            allRewards(page: $page, filter: {campaign_id: $campaign_id}) {
                 name
+                description
+                image
+                quantity
+                points
+                capPerAmbassador
             }
         }
     `,
     {
-        options: () => ({ variables: { page: 0 } }),
+        options: () => ({ variables: { page: 0, campaign_id: "001" } }),
         props: ({ data = {} }) => ({ allRewards: data.allRewards || [] }),
     },
 );
